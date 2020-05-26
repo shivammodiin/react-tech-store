133213ab593d38a1965078fde63cd963d4fe9c72
Shivam Modi <shivammodi456@gmail.com>
2020-05-26 17:46:37 +0530

    SideCart Done..

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
 import { ProductContext } from "../context/context";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export class SideCart extends Component {
   static contextType = ProductContext;
 
   render() {
-    const { cartOpen, handleCart } = this.context;
+    const { cartOpen, handleCart, cart, cartTotal } = this.context;
 
     return (
       <CartWrapper show={cartOpen} onClick={handleCart}>
-        <h4>Side CArt</h4>
+        <ul>
+          {cart.map((item) => {
+            return (
+              <li key={item.id} className="cart-item items-cart mb-4">
+                <img
+                  width="50"
+                  height="50"
+                  src={`../${item.image}`}
+                  alt="cart item"
+                />
+
+                <div className="ml-4">
+                  <h6 className="text-uppercase">{item.title}</h6>
+
+                  <h6 className="text-title text-capitalize">
+                    amount :{item.count}
+                  </h6>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+
+        <h4 className="text-capitalize text-center text-main">
+          cart total : ${cartTotal}
+        </h4>
+
+        <div className="text-center my-5">
+          <Link to="/cart" className="main-link">
+            cart page
+          </Link>
+        </div>
       </CartWrapper>
     );
   }
@@ -32,5 +64,16 @@ const CartWrapper = styled.nav`
 
   @media (min-width: 576px) {
     width: 20rem;
+  }
+  overflow: scroll;
+  padding: 1.5rem;
+
+  ul {
+    list-style: none;
+    padding: 0 !important;
+  }
+  .items-cart {
+    display: flex;
+    margin-right: 0.5rem;
   }
 `;
