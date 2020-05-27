99ee2a658f8f015fdff38a6f5c42aa688eda5993
Shivam Modi <shivammodi456@gmail.com>
2020-05-28 00:10:52 +0530

    Contact Us

---
+++ src/components/ContactPage/Contact.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+import Title from "../Title";
+
+class Contact extends Component {
+  render() {
+    return (
+      <>
+        <section className="py-5">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 my-3">
+              <Title title="contact us" />
+              <form
+                className="mt-5"
+                action="https://formspree.io/xbjzgdyg"
+                method="POST"
+              >
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="firstName"
+                    className="form-control"
+                    placeholder="Shivam Modi"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="yourname@gmail.com"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="subject"
+                    className="form-control"
+                    placeholder="Subject"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    className="form-control"
+                    placeholder="Message"
+                    row="10"
+                    required
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="submit"
+                    value="Send Message"
+                    className="btn btn-primary"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
+
+export default Contact;
